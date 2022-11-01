7fbf3d0b225ecb2759c60b7ccb32857c12611362
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-31 23:31:40 -0400

    registro e login

---
+++ README-install.md
@@ -0,0 +1,6 @@
+yarn create react-app .
+yarn add firebase
+https://www.youtube.com/watch?v=9bXhf_TELP4&list=PLpPqplz6dKxUfvM22GXRvYA4s-mvJE0XF&index=2&t=1s
+
+26:00 minutos
+athentication-tutorial no firebase
--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { auth } from './firebase-config'
 
 function App() {
+  const [registerEmail, setRegisterEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const [user, setUser] = useState({})
+
+  // onAuthStateChanged(auth, (currentUser) => {
+  //   setUser(currentUser)
+
+  // });
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+  }, [])
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const logout = async () => {
+    await signOut(auth)
+  }
+
   return (
     <div className="App">
       <div>
         <h3>Register User</h3>
-        <input type="text" placeholder="Email..." />
-        <input type="text" placeholder="Senha..." />
-        <button>Create User</button>
+        <input type="text" placeholder="Email..." onChange={(e) => setRegisterEmail(e.target.value)} />
+        <input type="text" placeholder="Senha..." onChange={(e) => setRegisterPassword(e.target.value)} />
+        <button onClick={register}>Create User</button>
       </div>
       <div>
         <h3>Login</h3>
-        <input type="text" placeholder="Email..." />
-        <input type="text" placeholder="Senha..." />
-        <button>Login User</button>
+        <input type="text" placeholder="Email..." onChange={(e) => setLoginEmail(e.target.value)} />
+        <input type="text" placeholder="Senha..." onChange={(e) => setLoginPassword(e.target.value)} />
+        <button onClick={login}>Login User</button>
       </div>
-      <h4>User Logged In:</h4>
-      <button>Sign Out</button>
+      <h4>User Logged In:</h4> {user?.email}
+      <button onClick={logout}>Sign Out</button>
     </div>
   );
 }
